d16da3bab827bba2b99c5a320bee81d8195517b0
coherent17 <mnb51817@gmail.com>
2021-08-21 14:36:25 +0800

    function headfile

--- function.h
+++ function.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int **constructTruthTable();
 void printTruthTable(int **);
 int *TensorProduct(int, int);
+void printTensorProductResult(int *);
+int *matrixMultiply(int *, int **);
+int **constructMatrix();
+bool checkArray(int *, int *);
+int **findMatrix(int **);
