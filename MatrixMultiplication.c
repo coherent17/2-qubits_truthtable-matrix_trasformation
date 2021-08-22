8341c2459287f367aa064ff00f80db20c203bb66
coherent17 <mnb51817@gmail.com>
2021-08-22 14:37:52 +0800

    fix the matrix multiplication

--- MatrixMultiplication.c
+++ MatrixMultiplication.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *matrixMultiply(int *array, int **matrix){
+int *matrixMultiply(int **matrix, int *array){
     int *result=malloc(sizeof(int)*4);
-    result[0]=array[0]*(matrix[0][0]+matrix[1][0]+matrix[2][0]+matrix[3][0]);
-    result[1]=array[1]*(matrix[0][1]+matrix[1][1]+matrix[2][1]+matrix[3][1]);
-    result[2]=array[2]*(matrix[0][2]+matrix[1][2]+matrix[2][2]+matrix[3][2]);
-    result[3]=array[3]*(matrix[0][3]+matrix[1][3]+matrix[2][3]+matrix[3][3]);
+    result[0]=matrix[0][0]*array[0]+matrix[0][1]*array[1]+matrix[0][2]*array[2]+matrix[0][3]*array[3];
+    result[1]=matrix[1][0]*array[0]+matrix[1][1]*array[1]+matrix[1][2]*array[2]+matrix[1][3]*array[3];
+    result[2]=matrix[2][0]*array[0]+matrix[2][1]*array[1]+matrix[2][2]*array[2]+matrix[2][3]*array[3];
+    result[3]=matrix[3][0]*array[0]+matrix[3][1]*array[1]+matrix[3][2]*array[2]+matrix[3][3]*array[3];
     return result;
 }
 
@@ -27,7 +27,7 @@ int main(){
     }
 
     matrix[0][2]=1;
-    matrix[3][0]=1;
+    matrix[3][0]=3;
     matrix[2][2]=1;
     
     int *result=matrixMultiply(array, matrix);
